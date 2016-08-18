e1cf19989c64f6cdfb6c40563667750efd4bf8d4
[01;31m[K"name"[m[K: "Laurie Skelly <[01;31m[K"email"[m[K: "laurieskelly@users.noreply.github.com>
2016-08-18 18:27:53 -0500

    remove references to HTMLWhitespaceCleaner from parse helpers

--- src/tal_producer.rb
+++ src/tal_producer.rb
@@ -2,14 +2,11 @@ require 'nokogiri'
 
 
 
-# get html from a file
 def nokogiri_from_file(filename)
   html = File.read(filename)
-  clean_html = HTMLWhitespaceCleaner.clean(html)
-  Nokogiri.parse(clean_html)
+  Nokogiri.parse(html)
 end
 
-
 def nokogiri_from_url(url) 
-  Nokogiri.parse(HTMLWhitespaceCleaner.clean(open(url).read))
+  Nokogiri.parse(open(url).read)
 end
