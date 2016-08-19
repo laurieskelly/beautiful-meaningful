5e8e1921d2507e92a15448a8bd52201773564283
[01;31m[K"name"[m[K: "Laurie Skelly <[01;31m[K"email"[m[K: "laurieskelly@users.noreply.github.com>
2016-08-18 20:37:47 -0500

    it lives!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,31 @@
+require APP_ROOT.join('src','tal_parser.rb')
+
+fails = []
+
 1996.upto(2016) do |year|
 
-  Season.create({year: year})
+  parser=SeasonParser.new(year)
 
+  Season.create({year: year, datfile: parser.datfile})
+
+  parser.episode_divs.each do |seed|
+
+    ep_parser = EpisodeParser.new(seed)
+    episode = Episode.new(ep_parser.table_data)
+
+    if episode.valid? 
+      episode.save 
+    else
+      fails << {
+        year: year, 
+        episode: ep_parser.episode_number, 
+        fails: episode.errors.full_messages
+      }
+    end
+
+  end
 end
 
+require 'json'
+
+JSON.dump(fails, open(APP_ROOT.join('db','failfile.json'),'w'))
