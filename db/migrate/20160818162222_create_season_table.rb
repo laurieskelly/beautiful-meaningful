249892fcf94842b86b2871f109893ede9d100a5e
[01;31m[K"name"[m[K: "Laurie Skelly <[01;31m[K"email"[m[K: "laurieskelly@users.noreply.github.com>
2016-08-18 20:35:04 -0500

    new migrations

--- db/migrate/20160818162222_create_season_table.rb
+++ db/migrate/20160818162222_create_season_table.rb
@@ -2,6 +2,7 @@ class CreateSeasonTable < ActiveRecord::Migration
   def change
     create_table :seasons, {:id => false} do |t|
       t.primary_key       :year
+      t.string            :datfile
 
       t.timestamps(null: false)
     end
--- db/migrate/20160818162233_create_episode_table.rb
+++ db/migrate/20160818162233_create_episode_table.rb
@@ -1,4 +1,12 @@
 class CreateEpisodeTable < ActiveRecord::Migration
   def change
+    create_table :episodes, {:id => false} do |t|
+      t.primary_key     :episode_number
+      t.string          :title
+      t.string          :archive_url
+      t.date            :original_air_date
+
+      t.timestamps(null: false)
+    end
   end
 end
