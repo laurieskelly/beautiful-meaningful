c9867a80ca76610d6a0c9e3e1f709c7916939956
[01;31m[K"name"[m[K: "Laurie Skelly <[01;31m[K"email"[m[K: "laurieskelly@users.noreply.github.com>
2016-08-18 19:59:15 -0500

    tal_parser handles the key info on the main season page

--- src/tal_parser.rb
+++ src/tal_parser.rb
@@ -1,5 +1,6 @@
 class SeasonParser
 
+  attr_reader :datfile
   def initialize(year)
     @year = year
     @datfile = season_filename
@@ -7,7 +8,7 @@ class SeasonParser
   end
 
   def season_filename
-    Dir[DATA_ROOT.join('seasons', "season_#{year}.dat")]
+    DATA_ROOT.join('seasons', "season_#{@year}.dat")
   end
 
   def nokogiri_from_file(filename)
@@ -15,10 +16,60 @@ class SeasonParser
     Nokogiri.parse(html)
   end
 
+  def episode_divs 
+    @nk.css('div.episode-archive')
+  end
 
 end
 
 
-def nokogiri_from_url(url) 
-  Nokogiri.parse(open(url).read)
+class EpisodeParser
+
+  attr_reader :table_data
+  def initialize(seed_div)
+    @nk = seed_div
+    @table_data = {
+      title: self.title,
+      archive_url: self.archive_url,
+      original_air_date: self.air_date,
+      episode_number: self.episode_number
+    }
+  end  
+
+  def archive_url 
+    @nk.css('a.image').first.attr('href')
+  end
+
+  def air_date_string
+    @nk.css('h3').first.css('span').first.text
+  end
+
+  def air_date
+    convert_date(self.air_date_string)
+  end
+
+  def header_string
+    @nk.css('h3').first.css('a').first.text
+  end
+
+  def episode_number
+    self.header_string.split(':')[0].to_i
+  end
+  
+  def title
+    self.header_string.split(':')[1].remove(self.air_date_string).strip
+  end
+
+  private 
+  
+  # turns '07.25.2016' into '2016-07-25'
+  def convert_date(datestr)
+    components = datestr.split('.')
+    components.unshift(components.pop).join('-')
+  end
+
+  def nokogiri_from_url(url) 
+    Nokogiri.parse(open(url).read)
+  end
+
 end
